c6ef3351bcc98d4058003e23abee397b68101934
Jeff Chiu <jeffchiudev@gmail.com>
2025-07-10 22:04:29 -0700

    add logic to cause balloon to increase in size and pop after clicks_to_pop reaches 0

--- Balloon.gd
+++ Balloon.gd
@@ -6,4 +6,8 @@ var score_to_give : int = 1
 
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		print("increase size")
+		scale += Vector3.ONE * size_increase
+		clicks_to_pop -= 1
+		
+		if clicks_to_pop == 0:
+			queue_free()
