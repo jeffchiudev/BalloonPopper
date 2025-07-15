4068006ae076bf934a8425e75fc4f4e7947693d4
Jeff Chiu <jeffchiudev@gmail.com>
2025-07-14 20:32:13 -0700

    add @export to variables to change specs for each balloon

--- Balloon.gd
+++ Balloon.gd
@@ -1,8 +1,8 @@
 extends Area3D
 
-var clicks_to_pop : int = 3
-var size_increase : float = 0.2
-var score_to_give : int = 1
+@export var clicks_to_pop : int = 3
+@export var size_increase : float = 0.2
+@export var score_to_give : int = 1
 
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
